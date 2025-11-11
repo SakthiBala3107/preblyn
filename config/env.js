c76e65679edaa0c87911da41c2e49e081e0177a6
Azula <Azula@gmail.com>
2025-11-11 17:13:54 +0530

    [UPDATE] Completion of workflow logic and implementation of nodeMaild Package

--- config/env.js
+++ config/env.js
@@ -4,16 +4,13 @@ const envFile = `.env.${process.env.NODE_ENV || "development"}.local`;
 
 config({ path: envFile });
 
-// QSTASH_URL="https://qstash.upstash.io"
-// QSTASH_TOKEN="eyJVc2VySUQiOiJiMDRkOGVhZS1kZGY2LTQxMGItYTc3ZS04ZDkwMzkxYzc0OTkiLCJQYXNzd29yZCI6IjI2NTA1YjZhYjYyNjQyNzM4NTc5MDgwODEyOWFhNTdkIn0="
-// QSTASH_CURRENT_SIGNING_KEY="sig_7acuSgmaqyKYGU4iDeCckR5SQDdz"
-// QSTASH_NEXT_SIGNING_KEY="sig_5KPdb6LuNUveGYDPQUtd3PaJHp3g"
-
 // aizen aizen@gmail.com aizen
 //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTEyYWI4MmFiMTQwZDE2M2IzODIwM2IiLCJpYXQiOjE3NjI4MzEyMzQsImV4cCI6MTc2MzQzNjAzNH0.lfuVQHzXFlg-tP3ccbBCZyw-DPS_xvLmMgx-hlFpkjQ"
 
 export const {
   PORT,
+  SERVER_URL,
+  EMAIL_PASSWORD,
   NODE_ENV,
   DB_URI,
   ARCJET_KEY,
